096e81f17f7a54ef87b2d1df1cc8bfa8fd2682d1
Rebecca Wong <rebeccawong+github@stanford.edu>
2015-10-19 02:44:19 -0700

    Working on sending variable length arrays; begins with 'handshake' to Pi -- needs debugging

--- Rocket_Avionics/sendArray/sendArray.ino
+++ Rocket_Avionics/sendArray/sendArray.ino
@@ -1,27 +1,59 @@
 #include <Wire.h>
 #define SLAVE_ADDRESS 0x04
 
-long data[] = {5,100,24,123,125,11};
-int index = 0;
+byte data[1200];
+int index = 0;sss
+int state = 0;
+unsigned int i = 0;
+int number;
+
 void setup() {
     Serial.begin(9600); // start serial for output
   // Init i2c given address
     Wire.begin(SLAVE_ADDRESS);
     Wire.onRequest(sendData); 
+    Wire.onReceive(receiveData);
     
     Serial.print("Ready");
 }
 
 void loop() {
+  data[0] = i;
+  i++;
+  if(i<254){
+    data[i] = (byte)random(0,255);
+  }
+  
 }
 
 
 // callback for sending data
 
 void sendData() { 
+  switch(state){
+  case 2:
     Wire.write(data[index]);
     ++index;
-    if (index >= 6) {
+    if (index >= i-1) {
          index = 0;
     }
+   
+    break;
+
+  case 1:
+    Wire.write(1);
+    state = 2;
+    break;
+  }
+  }
+  
+
+void receiveData(int byteCount) {
+  while (Wire.available()) {
+    number = Wire.read();
+    if(number == 1){
+      state = 1;
+    }
+  }
  }
+
