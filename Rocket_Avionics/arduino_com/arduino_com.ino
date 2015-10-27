1af5ba24d221bd953ecb90141605ae05162a656b
Andrew <milichab@gmail.com>
2015-10-26 21:01:04 -0700

    cleaned up protocol

--- Rocket_Avionics/arduino_com/arduino_com.ino
+++ Rocket_Avionics/arduino_com/arduino_com.ino
@@ -29,12 +29,19 @@ float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
  * sends a 1 back to the pi and enters state 2. When it gets a read request
  * in state 2, it sends the data that it has saved. If at any time it
  * recieves a 1, it enters state 1 agian, sends the 1 back to the pi, and
- * enters stat 2.
+ * enters state 2.
  *
  *
  * The Pi will cycle through all connected i2c addresses
  * to obtain data from multiple boards. It will log and
  * process this data.
+ * 
+ * States: 
+ *    1: not active
+ *    2: sending and tracking data 
+ *    3: sending test data to reset pi location 
+ * 
+ * Change Log: 
  */
 
 #define SLAVE_ADDRESS0 0x05
@@ -51,35 +58,13 @@ boolean loggerOn = false;
 int sensorData[6];
 unsigned int index1 = 0;
 
-void initSensors()
-{
-  if (!accel.begin())
-  {
-    /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
-    while (1);
-  }
-  if (!mag.begin())
-  {
-    /* There was a problem detecting the LSM303 ... check your connections */
-    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
-    while (1);
-  }
-  if (!bmp.begin())
-  {
-    /* There was a problem detecting the BMP180 ... check your connections */
-    Serial.println("Ooops, no BMP180 detected ... Check your wiring!");
-    while (1);
-  }
-}
-
-
 void setup() {
   //  data[0] = 9;
 
   sensorData[0] = 5;
   pinMode(LED_BUILTIN, OUTPUT); // LED
   Serial.begin(9600); // start serial for output
+  
   // Init i2c given address
   Wire.begin(I2C_SLAVE, SLAVE_ADDRESS, I2C_PINS_18_19, I2C_PULLUP_EXT, I2C_RATE_400);
   Wire1.begin(I2C_SLAVE, SLAVE_ADDRESS1, I2C_PINS_29_30, I2C_PULLUP_EXT, I2C_RATE_400);
@@ -89,72 +74,6 @@ void setup() {
   Wire.onRequest(sendData);
   initSensors();
   Serial.println("Ready!");
-}
-
-
-
-/*
- * Collect the latest sensor data.
- */
-void collectData() {
-  sensors_event_t accel_event;
-  sensors_event_t mag_event;
-  sensors_event_t bmp_event;
-  sensors_vec_t   orientation;
-
-  /* Calculate pitch and roll from the raw accelerometer data */
-  accel.getEvent(&accel_event);
-  if (dof.accelGetOrientation(&accel_event, &orientation)) {
-    /* 'orientation' should have valid .roll and .pitch fields */
-    Serial.print(F("Roll: "));
-    Serial.print(orientation.roll);
-    sensorData[1] = orientation.roll;
-    Serial.print(F("; "));
-    Serial.print(F("Pitch: "));
-    Serial.print(orientation.pitch);
-    sensorData[2] = orientation.pitch;
-    Serial.print(F("; "));
-  }
-
-  /* Calculate the heading using the magnetometer */
-  mag.getEvent(&mag_event);
-  if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
-  {
-    /* 'orientation' should have valid .heading data now */
-    Serial.print(F("Heading: "));
-    Serial.print(orientation.heading);
-    sensorData[3] = orientation.heading;
-    Serial.print(F("; "));
-  }
-
-  /* Calculate the altitude using the barometric pressure sensor */
-  bmp.getEvent(&bmp_event);
-  if (bmp_event.pressure)
-  {
-    /* Get ambient temperature in C */
-    float temperature;
-    bmp.getTemperature(&temperature);
-    /* Convert atmospheric pressure, SLP and temp to altitude    */
-    Serial.print(F("Alt: "));
-    Serial.print(bmp.pressureToAltitude(seaLevelPressure,
-                                        bmp_event.pressure,
-                                        temperature));
-    sensorData[4] = bmp.pressureToAltitude(seaLevelPressure,
-                                           bmp_event.pressure,
-                                           temperature);
-    Serial.print(F(" m; "));
-    /* Display the temperature */
-    Serial.print(F("Temp: "));
-    Serial.print(temperature);
-    sensorData[5] = temperature;
-    Serial.print(F(" C"));
-  }
-
-  Serial.println(F(""));
-  delay(1000);
-
-
-  return;
 }
 
 // Used for keeping time
@@ -172,7 +91,7 @@ void loop() {
   //  for(int jj = 1 ; jj < 10 ; jj++) {
   //  data[jj] = random(0,254);
   //  }
-  collectData();
+  delay(100); 
 }
 
 /*
@@ -184,13 +103,7 @@ void receiveData(size_t byteCount) {
     Serial.print("data received: ");
     Serial.println(number);
 
-    if (number == 1) {
-      state = 1;
-      Serial.println("I HAVE BEEN ACTIVATED");
-    }
-    else {
-      // figure out what we want to do here
-    }
+    state = number; 
   }
 }
 
@@ -206,6 +119,8 @@ void receiveData(size_t byteCount) {
  *
  * TODO: a reset byte for index1 (can be upset with small errors now).
  */
+int testData[6] = {1,1,1,1,1,1};
+ 
 void sendData() {
   if (state == 1) {
     Wire.write(1);
@@ -221,6 +136,10 @@ void sendData() {
     if (index1 >= sizeof(sensorData) / sizeof(int)) {
       index1 = 0;
     }
+    collectData();
     return;
+  }
+  else if (state == 3){
+    Wire.write(1);
   }
 }
