38aeff1f9871ab574ae75c23749981cf07ab3b9a
Andrew <milichab@gmail.com>
2015-10-30 17:12:37 -0700

    Moved launch back

---
+++ Rocket_Avionics/launch.sh
@@ -0,0 +1,14 @@
+# edit /etc/rc.local to ~/Desktop/Code/Rocket_Avionics/launch.sh
+# put this file there
+
+#!/bin/sh
+sleep 10
+
+cd /
+cd home
+cd pi
+cd Desktop
+cd Code
+cd rockets
+cd Rocket_Avionics
+sudo python raspi.py
