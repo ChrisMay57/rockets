bb93b60709b0635453597cbb2b2d9c867f339ec9
Rebecca Wong <rebeccawong+github@stanford.edu>
2015-11-04 00:09:44 -0800

    Graph pitch roll heading on one graph

--- PiSky_Avionics/graphdata.sh
+++ PiSky_Avionics/graphdata.sh
@@ -2,7 +2,6 @@ set terminal png
 set output "10dofgraph.png"
 set title "10 DOF Sensor Data"
 set xlabel "Time (s)"
-set ylabel "Temp (degrees)"
 set datafile separator ","
-plot "log.csv" using 2:7
+plot "log.csv" using 2:3, "log.csv" using 2:4, "log.csv" using 2:5 
 set term x11
