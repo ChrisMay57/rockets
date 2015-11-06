5dfb4e32d09183711b8e7ac63ea8d718921acdb7
Andrew <milichab@gmail.com>
2015-11-05 23:41:50 -0800

    Copied imports into tendof file

--- PiSky_Avionics/Pi_10DOF/tendof.py
+++ PiSky_Avionics/Pi_10DOF/tendof.py
@@ -1,6 +1,6 @@
 from L3GD20 import L3GD20
-from BMP085 import BMP085
-from LSM303 import LSM303
+from Adafruit_BMP085 import BMP085
+from Adafruit_LSM303 import Adafruit_LSM303
 import smbus
 import time
 
