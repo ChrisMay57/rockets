f1b07e11398abc7f8775af37887f70dadde3ddfe
Andrew <milichab@gmail.com>
2015-11-09 12:52:40 -0800

    CSV logging added to 10dof pi

--- PiSky_Avionics/Pi_10DOF/sensor_test.py
+++ PiSky_Avionics/Pi_10DOF/sensor_test.py
@@ -1,8 +1,17 @@
 from tendof import * 
+import csv
 
 S = Unified_Sensor()
-while(True):
-	time.sleep(0.1)
-	print S.get_xyz()
-	print S.get_temperature()
-	print S.get_lsm()
+
+if __name__ == "__main__":
+	with open("log.csv", "a+") as CSVlog: 
+		while(True):
+			time.sleep(0.1)
+			xyz = S.get_xyz()
+			temp = S.get_temperature()
+			lsm = S.get_lsm()
+			data_line += xyz[1:-1] + "," + temp + "," + lsm
+			data_line += "\n"
+			print data_line 
+			# CSVlog.write(data_line) 
+			# implement above line after testing 
