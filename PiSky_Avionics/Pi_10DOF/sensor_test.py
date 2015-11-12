edff69b974f757c92c1e75121f0df892a066787d
Andrew <milichab@gmail.com>
2015-11-12 00:32:18 -0800

    wipe old data

--- PiSky_Avionics/Pi_10DOF/sensor_test.py
+++ PiSky_Avionics/Pi_10DOF/sensor_test.py
@@ -5,6 +5,10 @@ import csv
 S = Unified_Sensor()
 
 if __name__ == "__main__":
+	CSVfile = open('log.csv', 'w+')
+	CSVfile.truncate() # wipes file every time script is run
+	CSVfile.close()
+	
 	with open("log.csv", "a+") as CSVlog: 
 		while(True):
 			time.sleep(0.1)
