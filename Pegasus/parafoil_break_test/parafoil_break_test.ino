1514b591bc7c06da22c6f4ec29ca0b15c24c4aa4
Andrew <milichab@gmail.com>
2015-12-26 17:08:43 -0500

    New name, variables for LED

--- Pegasus/parafoil_break_test/parafoil_break_test.ino
+++ Pegasus/parafoil_break_test/parafoil_break_test.ino
@@ -7,9 +7,11 @@
  */
 
 const int stepsPerRevolution = 2048;
-
 boolean button_not_pressed_state = LOW;
 double pull_revs = 0.5;
+int stepper_speed = 60;
+int num_blinks = 10; // # of times to blink
+int final_delay = 1000; // final delay before moving motors
 
 Stepper m1(stepsPerRevolution, 0, 2, 1, 3);
 Stepper m2(stepsPerRevolution, 4, 6, 5, 7);
@@ -23,6 +25,10 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(button_pin, INPUT);
   pinMode(13, OUTPUT);
+
+  // set stepper speed 
+  m1.setSpeed(stepper_speed);
+  m2.setSpeed(stepper_speed);
 }
 
 // Break only once
@@ -37,7 +43,7 @@ void loop() {
   }
   if (moveMotors) {
     // waits for button push, then blinks 10 times in 5 seconds
-    for (int ii = 0; ii < 10; ii ++) {
+    for (int ii = 0; ii < num_blinks; ii ++) {
       digitalWrite(13, HIGH);
       delay(250);
       digitalWrite(13, LOW);
@@ -45,12 +51,13 @@ void loop() {
     }
     // sets LED to high for 1 second, then moves motors 
     digitalWrite(13, HIGH); 
-    delay(1000); // 1 second
+    delay(final_delay); // 1 second
     // step each at the same time for the number of times desired 
     for (int ii = 0; ii < int(stepsPerRevolution * pull_revs); ii ++) {
       m1.step(1);
       m2.step(1);
     }
+    moveMotors = false; 
   } else {
     digitalWrite(13, LOW);
   }
