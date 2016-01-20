b9bb66d2faeb2b4c2eda66beb021d916a18be2fd
Andrew <milichab@gmail.com>
2016-01-19 22:23:56 -0800

    Added serial stuff

--- Python Server/serve_data.py
+++ Python Server/serve_data.py
@@ -1,10 +1,24 @@
 import os
 import sys
+import time
+import serial
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
 
 """
     SSI rockets text based ground station. 
 """
+
+"""
+    Serial communication and logging. 
+"""
+xbee = serial.Serial(
+        port='/dev/tty/1', # figure out what this is 
+        baudrate=9600,
+        parity=serial.PARITY_ODD,
+        stopbits=serial.STOPBITS_TWO,
+        bytesize=serial.SEVENBITS
+    )
+filename = "xbee_log.txt"
 
 app = Flask(__name__)
 app.secret_key = 'xbf\xcb7\x0bv\xcf\xc0N\xe1\x86\x98g9\xfei\xdc\xab\xc6\x05\xff%\xd3\xdf'
