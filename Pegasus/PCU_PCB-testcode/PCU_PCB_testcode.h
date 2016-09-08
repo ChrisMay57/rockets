5188b82789c07e8528169a9dcd6dc42c40a1330d
john <johnldean0@gmail.com>
2016-09-08 00:24:42 -0700

    started the PCU Main avionics PCB test code project in eclipse

---
+++ Pegasus/PCU_PCB-testcode/PCU_PCB_testcode.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _PCU_PCB_testcode_H_
+#define _PCU_PCB_testcode_H_
+#include "Arduino.h"
+//add your includes for the project PCU_PCB_testcode here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project PCU_PCB_testcode here
+
+
+
+
+//Do not add code below this line
+#endif /* _PCU_PCB_testcode_H_ */
